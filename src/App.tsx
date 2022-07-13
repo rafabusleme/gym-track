1bf98cbde2f840e1db3970e03607437ffdf3784b
rafabusleme <raabusleme@uc.cl>
2022-07-13 11:30:36 -0400

    chore: wrap app inside of auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import AppRoutes from './routes/AppRoutes';
+import AuthContextProvider from './shared/contexts/AuthContext/AuthContext';
 
 import './App.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AuthContextProvider>
+        <AppRoutes />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
