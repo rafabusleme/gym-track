e2b30c4c7edf6260559154e735d7ac971186305a
rafabusleme <raabusleme@uc.cl>
2022-07-13 09:52:57 -0400

    chore: config firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCj0RPyxnKczPmGUPHUeQNvjojgxHn6VbI',
+  authDomain: 'gym-track-9731f.firebaseapp.com',
+  projectId: 'gym-track-9731f',
+  storageBucket: 'gym-track-9731f.appspot.com',
+  messagingSenderId: '1048732603305',
+  appId: '1:1048732603305:web:0a13d30566df4d79b92ab8',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './config/firebase';
 
 import './index.scss';
 
